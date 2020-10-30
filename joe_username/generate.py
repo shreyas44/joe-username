346498808775ef91a93b4be863e9924ef75d287b
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-30 22:16:43 +0530

    Username generator

---
+++ joe_username/generate.py
@@ -0,0 +1,23 @@
+import random
+
+JOES_CONSTANTS = ["right", "awesome", "interesting", "cool"]
+
+def generate(max_limit=4):
+  try:
+    max_limit = int(max_limit)
+    
+    if max_limit <= 0:
+      raise ValueError
+
+  except ValueError:
+
+    raise ValueError("Make sure you enter an integer greater than 0")
+
+  word = ""
+
+  for i in range(max_limit):
+    rand_word = random.choice(JOES_CONSTANTS)
+    word += rand_word[0].upper() + rand_word[1:]
+
+  return word
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+  name="joe-username",
+  version="0.1.0",
+  packages=["joe_username"],
+  license="MIT",
+  author="Shreyas",
+  author_email="shreyas.sreenivasa@gmail.com",
+  url="https://github.com/shreyas44/joe-username",
+  description="Generate a bunch if words Joe says",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT",
+    "Operating System :: OS Independent",
+  ],
+  python_requires=">3.6.0",
+  entry_points={
+    "console_scripts": [
+      "joe-username = joe_username.__main__:main"
+    ]
+  }
+)
